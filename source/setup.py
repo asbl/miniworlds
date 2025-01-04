a3fde09a4dcf56aff06f63b80e281d8d91386453
Andreas Siebel <andreas.siebel@it-teaching.de>
2025-01-04 19:41:54 +0100

    pushed minor version, extended preparation for pyodide

--- source/setup.py
+++ source/setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="miniworlds",
-    version="3.0.1.3",
+    version="3.0.1.4",
     description="Create 2D worlds and Games",
     long_description=long_description,
     long_description_content_type="text/markdown",
