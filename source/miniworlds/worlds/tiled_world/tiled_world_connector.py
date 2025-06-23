97ada9a7515d599711d9602bc959643a6a53e821
Andreas Siebel <andreas.siebel@it-teaching.de>
2025-06-23 23:22:02 +0200

    refactored camera manager and tiled camera manager

--- source/miniworlds/worlds/tiled_world/tiled_world_connector.py
+++ source/miniworlds/worlds/tiled_world/tiled_world_connector.py
@@ -4,37 +4,72 @@ import miniworlds.worlds.manager.world_connector as world_connector
 
 
 class TiledWorldConnector(world_connector.WorldConnector):
-    def __init__(self, world, actor):
-        super().__init__(world, actor)
+    """
+    WorldConnector implementation for TiledWorlds.
+    Handles actor integration into static actor structures and provides
+    the appropriate sensor and position manager classes.
+    """
 
     @staticmethod
     def get_sensor_manager_class():
+        """
+        Returns:
+            The class used for sensor management in tiled worlds.
+        """
         return tiledworldsensor.TiledWorldSensorManager
 
     @staticmethod
     def get_position_manager_class():
+        """
+        Returns:
+            The class used for position management in tiled worlds.
+        """
         return tiledpositionmanager.TiledWorldPositionManager
 
-    def remove_actor_from_world(self, kill = False):
+    def remove_actor_from_world(self, kill=False):
+        """
+        Removes the actor from both static and dynamic world representations.
+
+        Args:
+            kill (bool): Whether the actor should be permanently removed.
+        """
         self.remove_static_actor()
         self.remove_dynamic_actor()
         super().remove_actor_from_world()
 
     def add_static_actor(self):
-        if self.actor not in self.world.static_actors_dict[self.actor.position]:
-            self.world.static_actors_dict[self.actor.position].append(self.actor)
+        """
+        Adds the actor to the static actor list at its current position,
+        and queues it for costume reload if necessary.
+        """
+        pos = self.actor.position
+        # Ensure the position key exists
+        if pos not in self.world.static_actors_dict:
+            self.world.static_actors_dict[pos] = []
+
+        if self.actor not in self.world.static_actors_dict[pos]:
+            self.world.static_actors_dict[pos].append(self.actor)
             self.world.reload_costumes_queue.append(self.actor)
 
     def remove_static_actor(self):
-        if (
-            self.actor.position in self.world.static_actors_dict
-            and self.actor in self.world.static_actors_dict[self.actor.position]
-        ):
-            self.world.static_actors_dict[self.actor.position].remove(self.actor)
+        """
+        Removes the actor from the static actor list at its current position,
+        if present.
+        """
+        pos = self.actor.position
+        if pos in self.world.static_actors_dict:
+            if self.actor in self.world.static_actors_dict[pos]:
+                self.world.static_actors_dict[pos].remove(self.actor)
 
-    def set_static(self, value):
+    def set_static(self, value: bool):
+        """
+        Sets whether the actor is considered static in the world.
+
+        Args:
+            value (bool): Whether the actor is static.
+        """
         super().set_static(value)
-        if self.actor._static:
+        if getattr(self.actor, "_static", False):
             self.add_static_actor()
         else:
             self.remove_static_actor()
