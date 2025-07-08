9f60076f6cc0a9deea1dc29fa313fc0846d8753a
Andreas Siebel <andreas.siebel@it-teaching.de>
2025-07-08 17:26:26 +0200

    updated github workflow

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,7 @@ import sys
 
 sys.path.insert(0, os.path.abspath('../../'))
 sys.path.insert(0, os.path.abspath('../../miniworlds'))
-sys.path.insert(0, os.path.abspath('../source'))
+sys.path.insert(0, os.path.abspath('../../source'))
 
 # -- Project information -----------------------------------------------------
 
